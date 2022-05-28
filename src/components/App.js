e3fc83990341adb451e13e01a181474fb6579092
DannyLenk <93644643+DannyLenk@users.noreply.github.com>
2022-05-28 08:26:46 +0300

    fixed early restart bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,147 +1,126 @@
 import React from "react";
-import Preface from "./Preface"
-import Action from "./Action"
-import { nanoid } from 'nanoid'
+import Preface from "./Preface";
+import Action from "./Action";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [begin, setBegin] = React.useState(false);
+  const [apiData, setApiData] = React.useState("");
+  const [questions, setQuestions] = React.useState("");
+  const [checked, setChecked] = React.useState(false);
+  const [counter, setCounter] = React.useState(0);
 
-    const [begin, setBegin] = React.useState(false)
-    const [apiData, setApiData] = React.useState('')
-    const [questions, setQuestions] = React.useState('')
-    const [checked, setChecked] = React.useState(false)
-    const [counter, setCounter] = React.useState(0)
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+      .then((res) => res.json())
+      .then((data) => setApiData(data.results));
+  }, []);
 
-    React.useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
-            .then(res => res.json())
-            .then(data => setApiData(data.results))
-    }, [])
+  function setAnswers(correctAns, wrongAns, question) {
+    const random = Math.floor(Math.random() * 4);
 
-    function setAnswers(correctAns, wrongAns, question) {
-        const random = Math.floor(Math.random() * 4)
+    wrongAns.splice(random, 0, correctAns);
 
-        wrongAns.splice(random, 0, correctAns)
+    return wrongAns.map((ans) => ({
+      content: ans,
+      id: nanoid(),
+      isHeld: false,
+      question: question,
+      wrong: false,
+      right: false
+    }));
+  }
 
-        return wrongAns.map(ans => ({
-            content: ans,
-            id: nanoid(),
-            isHeld: false,
-            question: question,
-            wrong: false,
-            right: false
-        }))
-    }
+  function beginGame() {
+    setBegin(true);
+    setQuestions(translateApiData());
+  }
 
-    function beginGame() {
-        setBegin(true)
-        setQuestions(translateApiData())
-    }
+  function translateApiData() {
+    return apiData.map((item) => ({
+      id: nanoid(),
+      question: item.question,
+      correct: item.correct_answer,
+      answers: setAnswers(
+        item.correct_answer,
+        item.incorrect_answers,
+        item.question
+      )
+    }));
+  }
 
-    function translateApiData() {
-        return apiData.map(item => ({
+  function holdAnswer(id, ask) {
+    setQuestions((prevState) =>
+      prevState.map((question) => {
+        if (question.question !== ask) {
+          return question;
+        } else {
+          const answersHeld = question.answers.map((answer) =>
+            answer.id === id
+              ? { ...answer, isHeld: !answer.isHeld }
+              : { ...answer, isHeld: false }
+          );
 
-            id: nanoid(),
-            question: item.question,
-            correct: item.correct_answer,
-            answers: setAnswers(
-                item.correct_answer,
-                item.incorrect_answers,
-                item.question
-            )
-
-        }))
-    }
-
-    function holdAnswer(id, ask) {
-        setQuestions(prevState => prevState.map(question => {
-
-            if (question.question !== ask) {
-
-                return question
-
-            } else {
-
-                const answersHeld = question.answers.map(answer => (
-                    answer.id === id
-                        ? {...answer, isHeld: !answer.isHeld}
-                        : {...answer, isHeld: false}
-                ))
-
-                return {...question, answers: answersHeld}
-
-            }
-
-        } ))
-    }
-
-    function checkAnswers() {
-        setQuestions(prevState => prevState.map(item => {
-
-            const answersChecked = item.answers.map(ans => {
-
-                    if (ans.content === item.correct && ans.isHeld) {
-                        setCounter(prevState => prevState + 1)
-                        return {...ans, right: true, isHeld: false}
-                    } else if (ans.content !== item.correct && ans.isHeld) {
-                        return {...ans, wrong: true, isHeld: false}
-                    } else if (ans.content === item.correct && !ans.isHeld) {
-                        return {...ans, right: true}
-                    } else {
-                        return ans
-                    }
-
-                }
-            )
-
-            return {...item, answers: answersChecked}
-
-        }))
-
-        setChecked(true)
-        fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
-            .then(res => res.json())
-            .then(data => setApiData(data.results))
-    }
-
-    function playAgain() {
-
-        setApiData('')
-        setQuestions('')
-        setCounter(0)
-        setChecked(false)
-
-        if (apiData) {
-            beginGame()
+          return { ...question, answers: answersHeld };
         }
+      })
+    );
+  }
 
+  function checkAnswers() {
+    setQuestions((prevState) =>
+      prevState.map((item) => {
+        const answersChecked = item.answers.map((ans) => {
+          if (ans.content === item.correct && ans.isHeld) {
+            setCounter((prevState) => prevState + 1);
+            return { ...ans, right: true, isHeld: false };
+          } else if (ans.content !== item.correct && ans.isHeld) {
+            return { ...ans, wrong: true, isHeld: false };
+          } else if (ans.content === item.correct && !ans.isHeld) {
+            return { ...ans, right: true };
+          } else {
+            return ans;
+          }
+        });
+
+        return { ...item, answers: answersChecked };
+      })
+    );
+
+    setChecked(true);
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+      .then((res) => res.json())
+      .then((data) => setApiData(data.results));
+  }
+
+  function playAgain() {
+    setApiData("");
+    setQuestions("");
+    setCounter(0);
+    setChecked(false);
+    setBegin(false);
+
+    if (apiData) {
+      beginGame();
     }
+  }
 
-
-    return(
-
-        <main className="App">
-
-            {
-                begin
-
-                    ? <Action
-                        questions={questions}
-                        holdAnswer={holdAnswer}
-                        checkAnswers={checkAnswers}
-                        checked={checked}
-                        counter={counter}
-                        playAgain={playAgain}
-                    />
-
-                    : <Preface
-                        beginGame={beginGame}
-                        apiData={apiData}
-                    />
-            }
-
-        </main>
-
-    )
+  return (
+    <main className="App">
+      {begin ? (
+        <Action
+          questions={questions}
+          holdAnswer={holdAnswer}
+          checkAnswers={checkAnswers}
+          checked={checked}
+          counter={counter}
+          playAgain={playAgain}
+        />
+      ) : (
+        <Preface beginGame={beginGame} apiData={apiData} />
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,39 +1,33 @@
-import React from "react"
-import {decode} from 'html-entities';
+import React from "react";
+import { decode } from "html-entities";
 
 function Question(props) {
+  const answers = props.answers.map((ans) => (
+    <div
+      className="Answer"
+      key={ans.id}
+      style={{
+        backgroundColor: ans.isHeld
+          ? "#D6DBF5"
+          : ans.wrong
+          ? "#F8BCBC"
+          : ans.right
+          ? "#94D7A2"
+          : "white"
+      }}
+      onClick={() => props.holdAnswer(ans.id, ans.question)}
+    >
+      {decode(ans.content)}
+    </div>
+  ));
 
-    const answers = props.answers.map(ans => (
-        <div
-            className="Answer"
-            key={ans.id}
-            style={{
-                backgroundColor:
-                    ans.isHeld ? "#D6DBF5"
-                    : ans.wrong ? "#F8BCBC"
-                    : ans.right ? "#94D7A2"
-                    : "white"
-        }}
-            onClick={() => props.holdAnswer(ans.id, ans.question)}
-        >
-            {decode(ans.content)}
-        </div>
-    ))
+  return (
+    <section className="Question">
+      <h3 className="Question__ask">{decode(props.question)}</h3>
 
-
-    return(
-
-        <section className="Question">
-            <h3 className="Question__ask">
-                {decode(props.question)}
-            </h3>
-
-            <div className="Question__options">
-                {answers}
-            </div>
-        </section>
-
-    )
+      <div className="Question__options">{answers}</div>
+    </section>
+  );
 }
 
-export default Question
+export default Question;
